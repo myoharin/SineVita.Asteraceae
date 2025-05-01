721360d53a62e8b091865d3d4fc7e7eb63c23bae
MyoHarin <myoharin@gmail.com>
2025-05-01 19:15:57 +0100

    Adding new Asteraceae Frameworks

--- obj/Debug/net8.0/SineVita.Asteraceae.AssemblyInfo.cs
+++ obj/Debug/net8.0/SineVita.Asteraceae.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SineVita.Asteraceae")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67f2d98c291275e630ca32e9c722a6a28cd5905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0439c8ff180067ad75cc0d11714cd43c8aa21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SineVita.Asteraceae")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SineVita.Asteraceae")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
